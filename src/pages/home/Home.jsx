8e29d157fc0aaff8d93db97e637702fce399a8a4
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-20 15:58:42 +0530

    add componenets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,21 @@
 import React, { useContext } from "react";
 import Context from "../../context/data/Context";
+import HeroSection from "../../Components/herosection/HeroSection";
+import Filter from "../../Components/filter/Filter";
+import ProductCard from "../../Components/productcard/ProductCard";
+import Categeroy from "../../Components/Categeory/Categeroy";
 
 function Home() {
   const context = useContext(Context);
   console.log(context);
-  return <div>Home Page</div>;
+  return (
+    <>
+      <HeroSection />
+      {/* <Filter /> */}
+      <ProductCard />
+      <Categeroy />
+    </>
+  );
 }
 
 export default Home;
