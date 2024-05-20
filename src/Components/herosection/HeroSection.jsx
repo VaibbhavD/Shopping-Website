f030ff8e29c560900a0b9d98c2e9e3f96a09f33f
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-20 19:33:47 +0530

    add Dark Mode

--- src/Components/herosection/HeroSection.jsx
+++ src/Components/herosection/HeroSection.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import Context from "../../context/data/Context";
+
 function HeroSection() {
+  const context = useContext(Context);
+  const { mode } = context;
+
   return (
-    <div>
+    <div style={{ background: mode == "dark" ? "rgb(46 49 55)" : "" }}>
       <img
         src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
         alt=""
