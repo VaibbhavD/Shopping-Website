06d9f6904274eb279cc2b603ce6000b76d082d28
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-20 22:01:09 +0530

    Productcard change some style

--- src/Components/productcard/ProductCard.jsx
+++ src/Components/productcard/ProductCard.jsx
@@ -32,7 +32,7 @@ function ProductCard() {
           {pics.map((url) => (
             <li>
               <div
-                className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
+                className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
                 style={{
                   backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
                   color: mode === "dark" ? "white" : "",
