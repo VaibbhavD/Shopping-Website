70a5f47bda900d1a9ab57d46bc3040b2fc037983
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-22 14:58:59 +0530

    style changes opacity and padding margin

--- src/Components/modal/Modal.jsx
+++ src/Components/modal/Modal.jsx
@@ -31,7 +31,11 @@ export default function Modal(props) {
       </div>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => console.log("click model close ")}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -41,11 +45,11 @@ export default function Modal(props) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="flex min-h-full items-center justify-center p-4">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -55,84 +59,143 @@ export default function Modal(props) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl p-2  text-left align-middle shadow-xl transition-all bg-gray-50">
-                  <section className="">
-                    <div className="flex flex-col items-center justify-center py-8 mx-auto  lg:py-0">
-                      <div className="w-full  rounded-lg md:mt-0 sm:max-w-md xl:p-0 ">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                          <form className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                              <label
-                                htmlFor="name"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                              >
-                                Enter Full Name
-                              </label>
-                              <input
-                                type="name"
-                                name="name"
-                                id="name"
-                                className=" border outline-0 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-100"
-                                required
-                              />
-                            </div>
-                            <div>
-                              <label
-                                htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                              >
-                                Enter Full Address
-                              </label>
-                              <input
-                                type="text"
-                                name="address"
-                                id="address"
-                                className=" border outline-0 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-100"
-                                required
-                              />
-                            </div>
-                            <div>
-                              <label
-                                htmlFor="pincode"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                              >
-                                Enter Pincode
-                              </label>
-                              <input
-                                type="text"
-                                name="pincode"
-                                id="pincode"
-                                className=" border outline-0 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-100"
-                                required
-                              />
-                            </div>
-                            <div>
-                              <label
-                                htmlFor="mobileNumber"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                              >
-                                Enter Mobile Number
-                              </label>
-                              <input
-                                type="text"
-                                name="mobileNumber"
-                                id="mobileNumber"
-                                className=" border outline-0 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-100"
-                                required
-                              />
-                            </div>
-                          </form>
-                          <button
-                            onClick={closeModal}
-                            type="button"
-                            className="focus:outline-none w-full text-white bg-violet-600 bg-green-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 "
-                          >
-                            Order Now
-                          </button>
-                        </div>
+                <Dialog.Panel>
+                  <div class="leading-loose md:pt-20 ">
+                    <form
+                      class={`max-w-xl md:m-4 m-2 md:px-8 p-5 border-1 border-white rounded shadow-xl ${
+                        mode === "dark" ? "bg-gray-400 text-black" : "bg-white"
+                      }`}
+                    >
+                      <p class="text-gray-800  text-center font-bold">
+                        Customer information
+                      </p>
+                      <div class="">
+                        <label
+                          class="block text-sm font-medium "
+                          for="cus_name"
+                        >
+                          Name
+                        </label>
+                        <input
+                          class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                          id="cus_name"
+                          name="cus_name"
+                          type="text"
+                          required
+                          placeholder="Your Name"
+                          aria-label="Name"
+                        />
                       </div>
-                    </div>
-                  </section>
+                      <div class="mt-2">
+                        <label class=" font-medium " for="cus_email">
+                          Email
+                        </label>
+                        <input
+                          class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                          id="cus_email"
+                          name="cus_email"
+                          type="email"
+                          required
+                          placeholder="Your Email"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="mt-2">
+                        <label class=" font-medium " for="cus_email">
+                          Mobile No
+                        </label>
+                        <input
+                          class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                          id="cus_mobileno"
+                          name="cus_email"
+                          type="number"
+                          required
+                          placeholder="Your Email"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="mt-2">
+                        <label class=" block font-medium " for="cus_email">
+                          Address
+                        </label>
+                        <input
+                          class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                          id="cus_email"
+                          name="cus_email"
+                          type="text"
+                          required
+                          placeholder="Street"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="mt-2">
+                        <label
+                          class="hidden font-medium block "
+                          for="cus_email"
+                        >
+                          City
+                        </label>
+                        <input
+                          class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                          id="cus_email"
+                          name="cus_email"
+                          type="text"
+                          required
+                          placeholder="City"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="inline-block mt-2 w-1/2 pr-1">
+                        <label
+                          class="hidden block font-medium "
+                          for="cus_email"
+                        >
+                          State
+                        </label>
+                        <input
+                          class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                          id="cus_email"
+                          name="cus_email"
+                          type="text"
+                          required
+                          placeholder="State"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="inline-block mt-2 -mx-1 pl-1 w-1/2">
+                        <label
+                          class="hidden md:block font-medium "
+                          for="cus_email"
+                        >
+                          Zip
+                        </label>
+                        <input
+                          class="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
+                          id="cus_email"
+                          name="cus_email"
+                          type="text"
+                          required
+                          placeholder="Zip"
+                          aria-label="Email"
+                        />
+                      </div>
+                      <div class="mt-4 justify-end flex gap-1">
+                        <button
+                          class="px-8 p-1 text-white font-bold tracking-wider bg-green-600 rounded"
+                          type="submit"
+                        >
+                          Pay
+                        </button>
+                        <button
+                          class="px-4 p-1 text-white font-bold tracking-wider bg-red-600 rounded"
+                          type="submit"
+                          onClick={() => closeModal()}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </form>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
