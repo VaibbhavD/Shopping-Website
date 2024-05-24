6a683250ad864339f4807a347f217f3ceb032f4d
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-24 19:17:34 +0530

    create Auth Slice and define states as is login and loin logout reducers

---
+++ src/redux/AuthSlice.jsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { isLoggedin: false, isAdmin: false };
+const User = JSON.parse(localStorage.getItem("User"));
+if (User) {
+  initialState.isLoggedin = true;
+  if (User.user.email == "adminvaibhav@gmail.com") {
+    initialState.isAdmin = true;
+  }
+}
+const AuthSlice = createSlice({
+  name: "AuthUser",
+  initialState,
+  reducers: {
+    Login(state, action) {
+      state.isLoggedin = true;
+      if (action.payload === "adminvaibhav@gmail.com") {
+        state.isAdmin = true;
+      }
+    },
+    Logout(state, action) {
+      state.isLoggedin = false;
+      state.isAdmin = false;
+    },
+  },
+});
+
+export const AuthActions = AuthSlice.actions;
+export default AuthSlice.reducer;
