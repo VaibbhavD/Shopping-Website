6dac19de95cb49e4c850a5ede8e1a0f50fdf8169
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-24 19:19:50 +0530

    remove context and add into app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { Store } from "./redux/Store.jsx";
+import ContextProider from "./context/data/ContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // <ContextProider>
   <Provider store={Store}>
     <App />
   </Provider>
+  // {/* </ContextProider> */}
 );
