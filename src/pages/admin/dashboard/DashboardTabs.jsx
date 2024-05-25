ce8791515a8f10348630d5be50e84dcfd7907361
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-25 18:09:48 +0530

    show product data dynamically

--- src/pages/admin/dashboard/DashboardTabs.jsx
+++ src/pages/admin/dashboard/DashboardTabs.jsx
@@ -6,11 +6,15 @@ import { FaUser, FaCartPlus } from "react-icons/fa";
 import { AiFillShopping, AiFillPlusCircle, AiFillDelete } from "react-icons/ai";
 import AddProduct from "../pages/AddProduct";
 import Modal from "../../../Components/modal/Modal";
+import PageLoader from "../../../Components/Loader/PageLoader";
+import Loader from "../../../Components/Loader/Loader";
 
 function DashboardTab() {
   const context = useContext(myContext);
-  const { mode } = context;
+  const { mode, products, loader } = context;
   let [isOpen, setIsOpen] = useState(false);
+
+  // console.log(products);
 
   function closeModal() {
     setIsOpen(false);
@@ -25,6 +29,7 @@ function DashboardTab() {
         className="container mx-auto pt-10 -mt-12 md:mt-0"
         style={{ backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "" }}
       >
+        {loader && <PageLoader />}
         <div className="tab container mx-auto ">
           <Tabs defaultIndex={0} className=" ">
             <TabList className="md:flex md:space-x-8 bg-  grid grid-cols-3 text-center    md:justify-end md:pr-5 pb-5 ">
@@ -87,10 +92,10 @@ function DashboardTab() {
                     </div>
                   </button>
                 </div>
-                <div className="relative overflow-x-auto md:px-10 ">
+                <div className="relative overflow-x-auto md:px-10 md:min-h-40 ">
                   <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400  ">
                     <thead
-                      className="text-xs border border-gray-600 text-black uppercase bg-gray-200 shadow-[inset_0_0_8px_rgba(0,0,0,0.6)]"
+                      className="text-xs border text-center border-gray-600 text-black uppercase bg-gray-200 shadow-[inset_0_0_8px_rgba(0,0,0,0.6)]"
                       style={{
                         backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
                         color: mode === "dark" ? "white" : "",
@@ -120,102 +125,107 @@ function DashboardTab() {
                         </th>
                       </tr>
                     </thead>
-                    <tbody className="">
-                      <tr
-                        className="bg-gray-50 border-b  dark:border-gray-700"
-                        style={{
-                          backgroundColor:
-                            mode === "dark" ? "rgb(46 49 55)" : "",
-                          color: mode === "dark" ? "white" : "",
-                        }}
-                      >
-                        <td
-                          className="px-6 py-4 text-black "
-                          style={{ color: mode === "dark" ? "white" : "" }}
+                    {products.map((product) => (
+                      <tbody key={product.date} className="text-center">
+                        <tr
+                          className="bg-gray-50 border-b  dark:border-gray-700"
+                          style={{
+                            backgroundColor:
+                              mode === "dark" ? "rgb(46 49 55)" : "",
+                            color: mode === "dark" ? "white" : "",
+                          }}
                         >
-                          1.
-                        </td>
-                        <th
-                          scope="row"
-                          className="px-6 py-4 font-medium text-black whitespace-nowrap"
-                        >
-                          <img
-                            className="w-16"
-                            src="https://dummyimage.com/720x400"
-                            alt="img"
-                          />
-                        </th>
-                        <td
-                          className="px-6 py-4 text-black "
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          Title
-                        </td>
-                        <td
-                          className="px-6 py-4 text-black "
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          ₹100
-                        </td>
-                        <td
-                          className="px-6 py-4 text-black "
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          pots
-                        </td>
-                        <td
-                          className="px-6 py-4 text-black "
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          12 Aug 2019
-                        </td>
-                        <td className="px-6 py-4">
-                          <div className=" flex gap-2">
-                            <div
-                              className=" flex gap-2 cursor-pointer text-black "
-                              style={{ color: mode === "dark" ? "white" : "" }}
-                            >
-                              <div>
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="w-6 h-6 text-red-600"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                  />
-                                </svg>
-                              </div>
-                              <div>
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="w-6 h-6 text-blue-500"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                                  />
-                                </svg>
+                          <td
+                            className="px-6 py-4 text-black "
+                            style={{ color: mode === "dark" ? "white" : "" }}
+                          >
+                            1.
+                          </td>
+                          <th
+                            scope="row"
+                            className="px-6 py-4 font-medium text-black whitespace-nowrap"
+                          >
+                            <img
+                              className="w-16 m-auto"
+                              src={product.imageUrl}
+                              alt="img"
+                            />
+                          </th>
+                          <td
+                            className="px-6 py-4 text-black "
+                            style={{ color: mode === "dark" ? "white" : "" }}
+                          >
+                            {product.title}
+                          </td>
+                          <td
+                            className="px-6 py-4 text-black "
+                            style={{ color: mode === "dark" ? "white" : "" }}
+                          >
+                            {product.price}
+                          </td>
+                          <td
+                            className="px-4 py-4 text-center text-black "
+                            style={{ color: mode === "dark" ? "white" : "" }}
+                          >
+                            {product.category}
+                          </td>
+                          <td
+                            className="px-6 py-4 text-black "
+                            style={{ color: mode === "dark" ? "white" : "" }}
+                          >
+                            {product.date}
+                          </td>
+                          <td className="px-6 py-4">
+                            <div className=" flex gap-2">
+                              <div
+                                className=" flex gap-2 cursor-pointer text-black "
+                                style={{
+                                  color: mode === "dark" ? "white" : "",
+                                }}
+                              >
+                                <div>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6 text-red-600"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                    />
+                                  </svg>
+                                </div>
+                                <div>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6 text-blue-500"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                    />
+                                  </svg>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
+                          </td>
+                        </tr>
+                      </tbody>
+                    ))}
                   </table>
                 </div>
               </div>
             </TabPanel>
+            {/* Order Details */}
             <TabPanel>
               {/* <Order order={order} setOrder={setOrder} setLoading={setLoading} /> */}
               <div className="relative overflow-x-auto pb-16 md:px-10">
@@ -359,6 +369,7 @@ function DashboardTab() {
                 </table>
               </div>
             </TabPanel>
+            {/* User Details */}
             <TabPanel>
               {/* <User addressInfo={addressInfo} setAddressInfo={setAddressInfo} setLoading={setLoading} /> */}
               <div className="relative overflow-x-auto pb-10 md:px-10">
