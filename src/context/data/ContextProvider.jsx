1abdb0c2af40397cd76c84d8d93b1a969297b2f6
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-25 09:46:04 +0530

    define product state and add product and get product method creates

--- src/context/data/ContextProvider.jsx
+++ src/context/data/ContextProvider.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Context from "./Context";
+import { QuerySnapshot, Timestamp, addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { fireDB } from "../../firebase/FirebaseConfig";
+import { toast } from "react-toastify";
 
 const ContextProider = (props) => {
   const [mode, Setmode] = useState("light");
@@ -14,6 +17,72 @@ const ContextProider = (props) => {
       document.body.style.backgroundColor = "white";
     }
   };
+            // Product State
+  const[product,Setproduct]=useState({
+    title:null,
+    brand:null,
+    price:null,
+    imageUrl:null,
+    category:null,
+    description:null,
+    time:Timestamp.now();
+    date:new Date().toLocaleString(
+      "en-US",
+      {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }
+    )
+  })
+
+  const AddProduct=async()=>{
+    if (product.title == null || product.price == null || product.imageUrl == null || product.category == null || product.description == null) {
+      return toast.error('Please fill all fields')
+    }
+    const productref=collection(fireDB,"Products")
+    Setloader(true)
+    try {
+      await addDoc(productref,product)
+      toast.success("Product Add Successfully")
+      Setloader(false)
+      
+    } catch (error) {
+      toast.error(error.message)
+      Setloader(false);
+      
+    }
+    
+
+  }
+
+  const[Products,SetProducts]=useState([])
+
+  const getProducts=async()=>{
+    Setloader(true)
+    try {
+      const q=query(
+        collection(fireDB,"products"),
+        orderBy("time")
+      )
+      const data=onSnapshot(q,(QuerySnapshot)=>{
+        let productsdata=[];
+        QuerySnapshot.forEach(data=>{
+          productsdata.push({...data,id:data.id})
+        })
+        SetProducts(productsdata)
+        Setloader(false)
+      })
+      return ()=>data;
+    } catch (error) {
+      console.log(error)
+      Setloader(False)
+    }
+  }
+
+  useEffect(()=>{
+    getProducts()
+  },[])
 
   return (
     <Context.Provider
@@ -22,10 +91,14 @@ const ContextProider = (props) => {
         toggle,
         loader,
         Setloader,
+        AddProduct,
+        Setproduct,
+        product
       }}
     >
       {props.children}
     </Context.Provider>
   );
 };
+
 export default ContextProider;
