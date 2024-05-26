2f7e423d9e9cba58f7c484a4bbb3feb82502518a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-26 14:25:10 +0530

    update some style

--- src/pages/home/FurnitureSection.jsx
+++ src/pages/home/FurnitureSection.jsx
@@ -6,7 +6,7 @@ function FurnitureSection() {
   const { mode } = context;
   return (
     <div
-      class="font-sans text-[#333]  max-md:max-w-md mx-auto pt-10 px-5 "
+      class="font-sans text-[#333]  max-md:max-w-md mx-auto pt-10 pb-7 "
       style={{
         backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
         color: mode === "dark" ? "white" : "",
@@ -16,31 +16,31 @@ function FurnitureSection() {
         Furnitures & Home
         <p class="h-1 w-20 mt-3 bg-pink-600 rounded"></p>
       </h2>
-      <div class="grid md:grid-cols-2 items-center md:gap-8 gap-6 px-10">
-        <div class="max-md:order-1 max-md:text-center">
-          <p class=" text-sm font-bold text-pink-600">
+      <div class="grid md:grid-cols-2 items-center md:gap-8 gap-6 ">
+        <div class="max-md:order-1 max-md:text-center px-10">
+          <p class="hidden md:block text-sm font-bold text-pink-600">
             World's Best Bestselling Furniture
           </p>
-          <h2 class="md:text-5xl text-3xl font-extrabold mb-4 md:!leading-[55px]">
+          <h2 class="md:text-5xl text-xl font-extrabold mb-4 md:!leading-[55px]">
             Up to 60% off | Best deals on furniture, home & more
           </h2>
-          <p class="mt-4 text-base text-gray-500 leading-relaxed">
+          <p class="hidden md:block  mt-4 text-base text-gray-500 leading-relaxed">
             Luxurious, handcrafted furniture for ultimate comfort and elegance.
             Stylish, durable furniture blending modern design with timeless
             quality. Premium furniture for unparalleled comfort and
             sophistication."
           </p>
           <button
-            className="p-2 mt-5 px-5  font-bold rounded-md text-white hover:text-black bg-[#CF5022]
+            className="md:p-2 md:mt-5 mt-2 md:px-5 p-1 font-bold rounded-md text-white hover:text-black bg-[#CF5022]
  hover:bg-stone-100 border-2 border-black"
           >
             Explore
           </button>
         </div>
-        <div class="md:h-[450px] p-2">
+        <div class="md:h-[450px] h-52  p-2">
           <img
             src="https://lh5.googleusercontent.com/A9-jKMv4zzUUvl39aHQZa_4aBIQ46Ia6VdhyrKhm1Agkvi-BsasFv0Ia4FuDR00YfRcPwu1UvVKmeaXfTTGoFZ1CYqbBwC_Ifs_tWizUS-v11G49L-Ay_3seSkeWKQY3fn5Rcg_G24y14aKyPQ"
-            class="w-full h-full object-contain shadow-[0_2px_22px_-10px_rgba(93,96,127,0.2)]"
+            class="w-full h-full  shadow-[0_2px_22px_-10px_rgba(93,96,127,0.2)]"
             alt="Dining Experience"
           />
         </div>
