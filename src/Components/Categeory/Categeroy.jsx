c0debfb0829f8f650bc05849ed4ccc8c3292fdb4
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-31 11:30:56 +0530

    clean code

--- src/Components/Categeory/Categeroy.jsx
+++ src/Components/Categeory/Categeroy.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import Context from "../../context/data/Context";
 import Loader from "../Loader/Loader";
+import { Link } from "react-router-dom";
 
 function Categeroy() {
   const context = useContext(Context);
@@ -21,22 +22,16 @@ function Categeroy() {
               Categeory
               <p class="h-1 w-20 mt-3 bg-pink-600 rounded"></p>
             </h2>
-            {/* <p
-              class="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block"
-              style={{ color: mode === "dark" ? "white" : "" }}
-            >
-              New Trendy Latest Fashion And Design
-            </p> */}
           </div>
         </div>
 
         <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8 md:px-5">
-          <a
-            href="#"
+          <Link
+            to={"/category/ cloths"}
             class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
           >
             <img
-              src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+              src="https://images-na.ssl-images-amazon.com/images/G/31/img21/Apparel-Events/Topbrands/Ingress-men._SS800_QL85_.jpg"
               loading="lazy"
               alt="Photo by Minh Pham"
               class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -44,34 +39,30 @@ function Categeroy() {
 
             <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-              VR
-            </span>
-          </a>
-          <a
-            href="#"
+            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg"></span>
+          </Link>
+          <Link
+            to={"/category/ mobiles"}
             class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
           >
             <img
-              src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000"
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/nbshagun/Clp/tiles/10May/D119951047_WLD_5Grevamp_March2024_Header_Mob_gif_1242x450.gif"
               loading="lazy"
               alt="Photo by Magicle"
-              class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              class="absolute inset-0 h-full w-full object-cover object-top transition duration-200 group-hover:scale-110"
             />
 
             <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-              Tech
-            </span>
-          </a>
+            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg"></span>
+          </Link>
 
-          <a
-            href="#"
+          <Link
+            to={"/category/ furniture"}
             class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
           >
             <img
-              src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=1000"
+              src="https://m.media-amazon.com/images/G/31/IMG20/Furniture/2023/Header/3000X700_bestseller._SX3000_QL85_.jpg"
               loading="lazy"
               alt="Photo by Martin Sanchez"
               class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -79,17 +70,15 @@ function Categeroy() {
 
             <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-              Dev
-            </span>
-          </a>
+            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg"></span>
+          </Link>
 
-          <a
-            href="#"
+          <Link
+            to={"/category/ electronics"}
             class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
           >
             <img
-              src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
+              src="https://img.global.news.samsung.com/in/wp-content/uploads/2020/06/Flipkart_page_desktopV2.jpg"
               loading="lazy"
               alt="Photo by Lorenzo Herrera"
               class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
@@ -97,10 +86,8 @@ function Categeroy() {
 
             <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-              Retro
-            </span>
-          </a>
+            <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg"></span>
+          </Link>
         </div>
       </div>
     </div>
