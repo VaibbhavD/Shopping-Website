74af0bfdd43ce83e86337f14f47e0a31447ffd0a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-05-31 09:41:17 +0530

    Add view more Button and navigate to mainproduct page

--- src/pages/productInfo/Rating.jsx
+++ src/pages/productInfo/Rating.jsx
@@ -904,15 +904,6 @@ const ProductReview1 = (props) => {
               </div>
             </div>
           </div>
-
-          <div class="md:mt-6 text-center">
-            <button
-              type="button"
-              class="mb-2 me-2 text-gray-800 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium  hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-            >
-              View more reviews
-            </button>
-          </div>
         </div>
       </section>
     </>
