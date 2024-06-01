b1c18f5b794908bf1e24b420b01b117047817e21
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-06-01 12:47:12 +0530

    All login toast using settimeout and use useEffect

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navber from "../Header/navbar";
 import Footer from "../Footer/footer";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import Context from "../../context/data/Context";
+import { useSelector } from "react-redux";
+import Modal from "../modal/Modal";
+import Login from "../../pages/registration/Login";
+import { toast } from "react-toastify";
 
 function Layout({ children }) {
+  const [isOpen, SetisOpen] = useState();
+
+  useEffect(() => {
+    setTimeout(() => {
+      toast.info("Login To Unlock More Features");
+    }, 10000);
+
+    return () => {
+      clearTimeout();
+    };
+  }, []);
+
   ScrollToTop();
+
   return (
     <div>
       <Navber />
